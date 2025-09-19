495ab3efb5fb75dec86f4390d70cbabd585729d9
MilosM00 <milos.milijasevic00@gmail.com>
2025-09-19 09:24:57 +0200

    login.php updated

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php include("partials/head.html"); ?>
+
+    <link rel="stylesheet" href="styles/login.css">
+
+    <title>Login | Eagle</title>
+</head>
+
+<body>
+
+    <div class="form-container">
+        <form method="POST">
+            <a href="index.php">
+                <img src="images/eagle-logo.webp" alt="Eagle" />
+            </a>
+            <input type="text" placeholder="Username..." name="" />
+            <input type="password" placeholder="Password..." name="" />
+            <button type="submit">Login</button>
+        </form>
+    </div>
+
+    <?php include("partials/admin.html"); ?>
+
+</body>
+
+</html>
