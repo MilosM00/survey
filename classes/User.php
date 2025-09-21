08f10c8b3f8e8eadfd46f008c142f7389f7dea76
MilosM00 <milos.milijasevic00@gmail.com>
2025-09-21 22:23:34 +0200

    User.php updated

--- classes/User.php
+++ classes/User.php
@@ -10,4 +10,45 @@ class User
         $database = new Database();
         $this->connection = $database->getConnection();
     }
+
+    public function userRegister($username, $password, $confirm_password)
+    {
+        if($password == $confirm_password)
+        {
+            $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+            $query = "INSERT INTO " . $this->table . " (user_username, user_password) VALUES(:username, :password)";
+            $stmt = $this->connection->prepare($query);
+            $stmt->bindParam(":username", $username);
+            $stmt->bindParam(":password", $hashed_password);
+            $stmt->execute();
+
+            header("location:login.php");
+            exit;
+        }
+
+        else
+        {
+            header("location:register.php");
+            exit;
+        }
+    }
+
+    public function userLogin($username, $password)
+    {
+        $query = "SELECT * FROM " . $this->table . " WHERE user_username = :username";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(":username", $username);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_OBJ);
+
+        if($user && password_verify($password, $user->user_password))
+        {
+            $_SESSION["logged_in"] = true;
+
+            header("location:dashboard.php");
+            exit;
+        }
+    }
 }
